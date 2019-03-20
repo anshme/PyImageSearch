f151d567e7ba838db4fb9bb75d309f537bf1d887
Anshuman <36338114+Ansh-me2@users.noreply.github.com>
2019-03-20 07:26:19 +0530

    Add files via upload

---
+++ try.py
@@ -0,0 +1,44 @@
+import imutils
+import cv2
+
+# load the input image and show its dimensions, keeping in mind that
+# images are represented as a multi-dimensional NumPy array with
+# shape no. rows (height) x no. columns (width) x no. channels (depth)
+image = cv2.imread("jp.png")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+
+# display the image to our screen -- we will need to click the window
+# open by OpenCV and press a key on our keyboard to continue execution
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+# access the RGB pixel located at x=50, y=100, keepind in mind that
+# OpenCV stores images in BGR order rather than RGB
+(B, G, R) = image[100, 50]
+print("R={}, G={}, B={}".format(R, G, B))
+
+# extract a 100x100 pixel square ROI (Region of Interest) from the
+# input image starting at x=320,y=60 at ending at x=420,y=160
+roi = image[60:160, 320:420]
+cv2.imshow("ROI", roi)
+cv2.waitKey(0)
+
+# resize the image to 200x200px, ignoring aspect ratio
+resized = cv2.resize(image, (200, 200))
+cv2.imshow("Fixed Resizing", resized)
+cv2.waitKey(0)
+
+# fixed resizing and distort aspect ratio so let's resize the width
+# to be 300px but compute the new height based on the aspect ratio
+r = 300.0 / w
+dim = (300, int(h * r))
+resized = cv2.resize(image, dim)
+cv2.imshow("Aspect Ratio Resize", resized)
+cv2.waitKey(0)
+
+# manually computing the aspect ratio can be a pain so let's use the
+# imutils library instead
+resized = imutils.resize(image, width=300)
+cv2.imshow("Imutils Resize", resized)
+cv2.waitKey(0)
